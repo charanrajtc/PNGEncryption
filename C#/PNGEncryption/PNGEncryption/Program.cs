5ab49f14d8e42eec816a491e3b9ec33b42c6dec1
git <git>
2014-10-13 16:53:45 -0500

    Update C# and Java libs

---
+++ C#/PNGEncryption/PNGEncryption/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace com.jheto.PNGEncryption
+{
+    public class PNGSerializer
+    {
+
+        private PNGSerializer() { }
+
+        public static byte[] encodeText(string message)
+        {
+            byte[] bytes = null;
+            try
+            {
+                bytes = System.Text.Encoding.UTF8.GetBytes(message);
+            }
+            catch (Exception e)
+            {
+                bytes = null;
+            }
+            return bytes;
+        }
+
+        public static string decodeText(byte[] bytes)
+        {
+            string message = null;
+            try
+            {
+                message = System.Text.Encoding.UTF8.GetString(bytes);
+            }
+            catch (Exception e)
+            {
+                message = null;
+            }
+            return message;
+        }
+
+        public static Bitmap encodeBinary(byte[] bytes)
+        {
+            Bitmap bmp = null;
+            try
+            {
+                int sqrt = (int)Math.Ceiling(Math.Sqrt(bytes.Length));
+                bmp = new Bitmap(sqrt, sqrt);
+                for (int w = 0, index = 0; w < sqrt; w++)
+                {
+                    for (int h = 0; h < sqrt; h++)
+                    {
+                        if (index < bytes.Length)
+                        {
+                            int A = (int)bytes[index];
+                            Color color = Color.FromArgb(A, 0, 0, 0);
+                            bmp.SetPixel(w, h, color);
+                        }
+                        else
+                        {
+                            Color color = Color.FromArgb(0, 255, 255, 255);
+                            bmp.SetPixel(w, h, color);
+                        }
+                        index++;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                bmp = null;
+            }
+            return bmp;
+        }
+
+        public static byte[] decodeBinary(Bitmap bmp)
+        {
+            byte[] bytes = null;
+            try
+            {
+                int wSize = bmp.Width, hSize = bmp.Height;
+                MemoryStream stream = new MemoryStream();
+                for (int w = 0; w < wSize; w++)
+                {
+                    for (int h = 0; h < hSize; h++)
+                    {
+                        Color color = bmp.GetPixel(w, h);
+                        if (color.R == 0 && color.G == 0 && color.B == 0) stream.WriteByte(color.A);
+                        else break;
+                    }
+                }
+                bytes = stream.ToArray();
+                stream = null;
+            }
+            catch (Exception e)
+            {
+                bytes = null;
+            }
+            return bytes;
+        }
+
+        public static byte[] bitmapToArray(Bitmap bmp)
+        {
+            byte[] byteArray = null;
+            try
+            {
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    bmp.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                    stream.Close();
+                    byteArray = stream.ToArray();
+                }
+            }
+            catch (Exception e)
+            {
+                byteArray = null;
+            }
+            return byteArray;
+        }
+
+        public static Bitmap arrayToBitmap(byte[] array)
+        {
+            Bitmap bmp = null;
+            try
+            {
+                using (MemoryStream ms = new MemoryStream(array))
+                {
+                    bmp = (Bitmap)Image.FromStream(ms);
+                }
+            }
+            catch (Exception e)
+            {
+                bmp = null;
+            }
+            return bmp;
+        }
+
+        public static void doIt()
+        {
+            try {
+                string str = @"X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*";
+                byte[] rawdata = PNGSerializer.encodeText(str);
+                Bitmap bmp = PNGSerializer.encodeBinary(rawdata);
+                bmp.Save("eicar.png", ImageFormat.Png);
+                byte[] serial = PNGSerializer.bitmapToArray(bmp);
+                bmp = PNGSerializer.arrayToBitmap(serial);
+                rawdata = PNGSerializer.decodeBinary(bmp);
+                string text = PNGSerializer.decodeText(rawdata);
+                Console.WriteLine(text);
+            }
+            catch (Exception e) { }
+        }
+    
+    }
+
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            PNGSerializer.doIt();
+        }
+    }
+}
---
+++ C#/PNGEncryption/PNGEncryption/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PNGEncryption")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PERSONAL")]
+[assembly: AssemblyProduct("PNGEncryption")]
+[assembly: AssemblyCopyright("Copyright © PERSONAL 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("91ea41a7-b5c8-4f4e-b9e2-aea84a5cf6ab")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
